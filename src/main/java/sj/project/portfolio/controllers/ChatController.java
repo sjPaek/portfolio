aa6eee48eb48a06bc81cf5da90904280a4d7baa9
xaexoon <xaexoon07@gmail.com>
2024-02-21 16:08:11 +0900

    no message

--- src/main/java/sj/project/portfolio/controllers/ChatController.java
+++ src/main/java/sj/project/portfolio/controllers/ChatController.java
@@ -1,27 +1,12 @@
 package sj.project.portfolio.controllers;
 
-import java.util.ArrayList;
-
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.event.EventListener;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.Payload;
-import org.springframework.messaging.handler.annotation.SendTo;
-import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
-import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
-import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.socket.messaging.SessionConnectEvent;
-import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 
 import lombok.extern.slf4j.Slf4j;
 import sj.project.portfolio.dao.models.ChatMessageModel;
-import sj.project.portfolio.models.MessageType;
-
 
 @Controller
 @Slf4j
--- src/main/java/sj/project/portfolio/dao/models/ChatMessageModel.java
+++ src/main/java/sj/project/portfolio/dao/models/ChatMessageModel.java
@@ -4,7 +4,6 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import sj.project.portfolio.models.MessageType;
 
 @Builder
 @Data
